e58fdec3f6862691290f09dd85d25acdee4382e5
Troy Oster <toster1011@me.com>
2020-02-10 14:03:21 -0700

    Upd: Stylistic Changes

--- src/components/AddForm.jsx
+++ src/components/AddForm.jsx
@@ -2,66 +2,104 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
-
 const FormContainer = styled.div`
-    margin: auto;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 30%;
+  width: 350px;
+  margin-top: 100px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  padding-bottom: 30px;
+`;
+
+const FormList = styled.ul`
+  list-style-type: none;
+  padding: 10px;
+`;
+
+const Label = styled.label`
+  font-size: 2em;
+  font-weight: bold;
+  display: inline-block;
+  margin-right: 20px;
+`;
+
+const ListElement = styled.li`
     display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    width: 30%;
-    height: 30%;
-    margin-top: 100px;
-    box-shadow:0 4px 8px 0 rgba(0,0,0,0.2);
-    padding-bottom: 30px;
+    justify-content: space-between;
+    padding: .6em;
+`;
+
+const CheckBox = styled.input.attrs({
+  type:'checkbox'
+})`
+    width: 30px;
+    height: 20px;
+    bottom: 1px;
+`;
+
+const SubmitButton = styled.button`
+  margin-left: 65%;
+  font-size: 1.1em;
+  height: 32px;
+  width: 30%;
 `;
 
 const AddForm = props => {
   return (
-    <FormContainer>
-        <h1 style={{}}>Add Item</h1>
+    <FormContainer className="formCard">
+      <h1>Add Item</h1>
       <form onSubmit={props.handleSubmit}>
-        <label>
-          Item:
-          <input
-            name="name"
-            type="text"
-            value={props.form.name}
-            onChange={props.handleChange}
-          />
-        </label>
-        <br />
-        <label>
-          Important:
-          <input
-            name="isImportant"
-            type="checkbox"
-            checked={props.form.isImportant}
-            onChange={props.handleChange}
-          />
-        </label>
-        <br />
-        <label>
-          For Yourself:
-          <input
-            name="isSelf"
-            type="checkbox"
-            checked={props.form.isSelf}
-            onChange={props.handleChange}
-          />
-        </label>
-        <br />
-        <label>
-          Urgent:
-          <input
-            name="isUrgent"
-            type="checkbox"
-            checked={props.form.isUrgent}
-            onChange={props.handleChange}
-          />
-        </label>
-        <br/>
-        <input type="submit" value="Submit" />
+        <FormList>
+          <ListElement>
+            <Label htmlFor="name">Item:</Label>
+            <input
+              id="name"
+              name="name"
+              type="text"
+              value={props.form.name}
+              onChange={props.handleChange}
+            />
+          </ListElement>
+          <ListElement>
+            <Label htmlFor="isImportant">
+              Important:
+              <CheckBox
+                id="isImportant"
+                name="isImportant"
+                type="checkbox"
+                checked={props.form.isImportant}
+                onChange={props.handleChange}
+              />
+            </Label>
+          </ListElement>
+          <ListElement>
+            <Label>
+              For Yourself:
+              <CheckBox
+                name="isSelf"
+                type="checkbox"
+                checked={props.form.isSelf}
+                onChange={props.handleChange}
+              />
+            </Label>
+          </ListElement>
+          <ListElement>
+            <Label>
+              Urgent:
+              <CheckBox
+                name="isUrgent"
+                type="checkbox"
+                checked={props.form.isUrgent}
+                onChange={props.handleChange}
+              />
+            </Label>
+          </ListElement>
+        </FormList>
+        <SubmitButton type="submit">Submit</SubmitButton>
       </form>
     </FormContainer>
   );
